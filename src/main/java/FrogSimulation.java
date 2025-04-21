6fa31ecf735524a649d0c84b0df9b3cc4f6fbf75
jnthnluong <jnthnluong@gmail.com>
2025-04-21 12:33:01 -0700

    Update FrogSimulation.java

--- src/main/java/FrogSimulation.java
+++ src/main/java/FrogSimulation.java
@@ -16,12 +16,26 @@ public class FrogSimulation
 	public boolean simulate()
 	{ 
 		/* to be implemented in part (a) */
+		int total = 0;
+		for(int i = 0; i < maxHops; i++){
+			total = total + hopDistance();
+			
+		}
+		return (total >= goalDistance);
 
 	}
 	
 	public double runSimulations(int num)
 	{ 
 		/* to be implemented in part (b) */ 
+		int successes = 0;
+		for(int i = 0; i < num; i++){
+			if(simulate() == true)
+				successes++;
+		
+		}
+		return((double)successes/(double)num);
+		
 
 	}
 	
